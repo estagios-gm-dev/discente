403bb0acf93916920e4995ce0786cd8bcb470761
Jabes <jabesr@gmail.com>
2019-03-24 15:47:07 -0300

    Updates

---
+++ precache-manifest.84bab9d24f797dbd7af469d4281500fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1186cdd312a903f3bfe9fc7f48fd4358",
+    "url": "./static/media/Inicio.1186cdd3.md"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a42dcb070eb5e8865fdf",
+    "url": "./static/js/main.34e36ec3.chunk.js"
+  },
+  {
+    "revision": "7c6430434e56e243aa2b",
+    "url": "./static/js/2.039fc3e1.chunk.js"
+  },
+  {
+    "revision": "d1009e9ae938e69a600a1766b72aa717",
+    "url": "./index.html"
+  }
+];
